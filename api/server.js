e631086d5e64f07ff453635aadc76657fe32d5c8
Standfirm <147502004+stanfy79@users.noreply.github.com>
2025-05-23 02:43:20 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve frontend
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // API route
